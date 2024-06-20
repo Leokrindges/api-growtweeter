fa9622d654c90e035a1676292eddd1a775122859
leonardo Krindges <krindgesleonardo3@gmail.com>
2024-06-19 22:47:02 -0300

    Adicionado configuracoes do projeto e model user

---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import { log } from "console";
+import cors from "cors";
+import "dotenv/config";
+import express from "express";
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.listen(process.env.PORT, () => {
+  console.log(`server running on port ${process.env.PORT}`);
+});
