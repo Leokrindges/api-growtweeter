b70ed6f616ccf8c340c3cf14e68a4432564897b2
leonardo Krindges <krindgesleonardo3@gmail.com>
2024-07-11 23:37:46 -0300

    Aletrado retornos das rotas delete, get e create

--- src/controllers/reply.controller.ts
+++ src/controllers/reply.controller.ts
@@ -34,21 +34,12 @@ export class ReplyController {
         },
       });
 
-      //conta quantos replys o tweet tem
-      const countReply = await prismaConnection.reply.count({
-        where: {
-          tweetId: tweetId,
-        },
-      });
-
       return res.status(201).json({
         ok: true,
         message: `Reply cadastrado com sucesso para o usuário ${
           (user as User).name
         }`,
-        originalTweet: tweetFound,
         replyTweet: createTweetReply,
-        totalReply: countReply,
       });
     } catch (err) {
       return res.status(500).json({
@@ -63,18 +54,13 @@ export class ReplyController {
   public static async get(req: Request, res: Response) {
     try {
       const { replyId } = req.params;
-      const { user } = req.body
+      const { user } = req.body;
 
       const replyFound = await prismaConnection.reply.findFirst({
         where: { tweet: { type: "R", id: replyId }, userId: (user as User).id },
         select: {
           tweet: true,
-          user: {
-            select: {
-              name: true,
-              username: true,
-            },
-          },
+          user: true,
         },
       });
 
@@ -100,68 +86,62 @@ export class ReplyController {
     }
   }
 
-  public static async update(req: Request, res: Response) {
-    try {
-      const { replyId } = req.params;
+  //COMO UM REPLY É UM TWEET O REPLY PODE SER ATUAIZADO PELA ROTA DE TWEETS, POR ISO COMENTEI A FUNÇÃO
 
-      const { content, user } = req.body;
+  // public static async update(req: Request, res: Response) {
+  //   try {
+  //     const { replyId } = req.params;
 
-      const replyFound = await prismaConnection.reply.findFirst({
-        where: { tweet: { type: "R", id: replyId }, userId: (user as User).id },
-        include: {
-          tweet: true,
-          user: {
-            select: {
-              name: true,
-              username: true,
-            },
-          },
-        },
-      });
+  //     const { content, user } = req.body;
 
-      if (!replyFound) {
-        return res.status(400).json({
-          ok: false,
-          message: "Reply não encontrado",
-        });
-      }
+  //     const replyFound = await prismaConnection.reply.findFirst({
+  //       where: { tweet: { type: "R", id: replyId }, userId: (user as User).id },
+  //       include: {
+  //         tweet: true,
+  //         user: {
+  //           select: {
+  //             name: true,
+  //             username: true,
+  //           },
+  //         },
+  //       },
+  //     });
 
-      const replyUpdated = await prismaConnection.reply.update({
-        where: {
-          id: replyFound.id,
-        },
-        data: {
-          tweet: {
-            update: { content: content },
-          },
-        },
-        include: {
-          user: {
-            select: { name: true, username: true },
-          },
-          tweet: true,
-        },
-      });
+  //     if (!replyFound) {
+  //       return res.status(400).json({
+  //         ok: false,
+  //         message: "Reply não encontrado",
+  //       });
+  //     }
 
-      return res.status(200).json({
-        ok: true,
-        message: "Reply atualizado",
-        replyUpdated,
-      });
-    } catch (err) {
-      return res.status(500).json({
-        ok: false,
-        message: `Ocorreu um erro inesperado. Erro: ${(err as Error).name} - ${
-          (err as Error).message
-        }`,
-      });
-    }
-  }
+  //     const replyUpdated = await prismaConnection.tweet.update({
+  //       where: {
+  //         id: replyFound.id,
+  //       },
+  //       data: {
+  //         content: content,
+  //       },
+  //     });
+
+  //     return res.status(200).json({
+  //       ok: true,
+  //       message: "Reply atualizado",
+  //       replyUpdated,
+  //     });
+  //   } catch (err) {
+  //     return res.status(500).json({
+  //       ok: false,
+  //       message: `Ocorreu um erro inesperado. Erro: ${(err as Error).name} - ${
+  //         (err as Error).message
+  //       }`,
+  //     });
+  //   }
+  // }
 
   public static async delete(req: Request, res: Response) {
     try {
       const { replyId } = req.params;
-      const { user } = req.body
+      const { user } = req.body;
 
       const replyFound = await prismaConnection.reply.findFirst({
         where: { tweetId: replyId, userId: (user as User).id },
@@ -180,14 +160,17 @@ export class ReplyController {
         },
       });
 
-      await prismaConnection.tweet.delete({
-        where: { id: replyId}
-      })
+      const replyDeleted = await prismaConnection.tweet.delete({
+        where: {
+          id: replyId,
+          AND: { type: "R", userId: (user as User).id },
+        },
+      });
 
       return res.status(200).json({
         ok: true,
         message: "Reply deletado com sucesso",
-        replyFound,
+        replyDeleted,
       });
     } catch (err) {
       return res.status(500).json({
